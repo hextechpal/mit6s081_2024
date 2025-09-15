25f351cc7e96ff119fee48f10881860c4fe476d8
Prashant Pal <pal.prashant31@gmail.com>
2025-09-15 12:08:10 +0100

    net: e1000

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -23,8 +23,7 @@ struct spinlock e1000_lock;
 // called by pci_init().
 // xregs is the memory address at which the
 // e1000's registers are mapped.
-void
-e1000_init(uint32 *xregs)
+void e1000_init(uint32 *xregs)
 {
   int i;
 
@@ -40,26 +39,28 @@ e1000_init(uint32 *xregs)
 
   // [E1000 14.5] Transmit initialization
   memset(tx_ring, 0, sizeof(tx_ring));
-  for (i = 0; i < TX_RING_SIZE; i++) {
+  for (i = 0; i < TX_RING_SIZE; i++)
+  {
     tx_ring[i].status = E1000_TXD_STAT_DD;
     tx_bufs[i] = 0;
   }
-  regs[E1000_TDBAL] = (uint64) tx_ring;
-  if(sizeof(tx_ring) % 128 != 0)
+  regs[E1000_TDBAL] = (uint64)tx_ring;
+  if (sizeof(tx_ring) % 128 != 0)
     panic("e1000");
   regs[E1000_TDLEN] = sizeof(tx_ring);
   regs[E1000_TDH] = regs[E1000_TDT] = 0;
-  
+
   // [E1000 14.4] Receive initialization
   memset(rx_ring, 0, sizeof(rx_ring));
-  for (i = 0; i < RX_RING_SIZE; i++) {
+  for (i = 0; i < RX_RING_SIZE; i++)
+  {
     rx_bufs[i] = kalloc();
     if (!rx_bufs[i])
       panic("e1000");
-    rx_ring[i].addr = (uint64) rx_bufs[i];
+    rx_ring[i].addr = (uint64)rx_bufs[i];
   }
-  regs[E1000_RDBAL] = (uint64) rx_ring;
-  if(sizeof(rx_ring) % 128 != 0)
+  regs[E1000_RDBAL] = (uint64)rx_ring;
+  if (sizeof(rx_ring) % 128 != 0)
     panic("e1000");
   regs[E1000_RDH] = 0;
   regs[E1000_RDT] = RX_RING_SIZE - 1;
@@ -67,32 +68,31 @@ e1000_init(uint32 *xregs)
 
   // filter by qemu's MAC address, 52:54:00:12:34:56
   regs[E1000_RA] = 0x12005452;
-  regs[E1000_RA+1] = 0x5634 | (1<<31);
+  regs[E1000_RA + 1] = 0x5634 | (1 << 31);
   // multicast table
-  for (int i = 0; i < 4096/32; i++)
+  for (int i = 0; i < 4096 / 32; i++)
     regs[E1000_MTA + i] = 0;
 
   // transmitter control bits.
-  regs[E1000_TCTL] = E1000_TCTL_EN |  // enable
-    E1000_TCTL_PSP |                  // pad short packets
-    (0x10 << E1000_TCTL_CT_SHIFT) |   // collision stuff
-    (0x40 << E1000_TCTL_COLD_SHIFT);
-  regs[E1000_TIPG] = 10 | (8<<10) | (6<<20); // inter-pkt gap
+  regs[E1000_TCTL] = E1000_TCTL_EN |                 // enable
+                     E1000_TCTL_PSP |                // pad short packets
+                     (0x10 << E1000_TCTL_CT_SHIFT) | // collision stuff
+                     (0x40 << E1000_TCTL_COLD_SHIFT);
+  regs[E1000_TIPG] = 10 | (8 << 10) | (6 << 20); // inter-pkt gap
 
   // receiver control bits.
-  regs[E1000_RCTL] = E1000_RCTL_EN | // enable receiver
-    E1000_RCTL_BAM |                 // enable broadcast
-    E1000_RCTL_SZ_2048 |             // 2048-byte rx buffers
-    E1000_RCTL_SECRC;                // strip CRC
-  
+  regs[E1000_RCTL] = E1000_RCTL_EN |      // enable receiver
+                     E1000_RCTL_BAM |     // enable broadcast
+                     E1000_RCTL_SZ_2048 | // 2048-byte rx buffers
+                     E1000_RCTL_SECRC;    // strip CRC
+
   // ask e1000 for receive interrupts.
-  regs[E1000_RDTR] = 0; // interrupt after every received packet (no timer)
-  regs[E1000_RADV] = 0; // interrupt after every packet (no timer)
+  regs[E1000_RDTR] = 0;       // interrupt after every received packet (no timer)
+  regs[E1000_RADV] = 0;       // interrupt after every packet (no timer)
   regs[E1000_IMS] = (1 << 7); // RXDW -- Receiver Descriptor Write Back
 }
 
-int
-e1000_transmit(char *buf, int len)
+int e1000_transmit(char *buf, int len)
 {
   //
   // Your code here.
@@ -102,7 +102,28 @@ e1000_transmit(char *buf, int len)
   // a pointer so that it can be freed after send completes.
   //
 
-  
+  // acquire(&e1000_lock);
+  int i = regs[E1000_TDT];
+  if (!(tx_ring[i].status & E1000_TXD_STAT_DD))
+  {
+    // ring is full
+    // release(&e1000_lock);
+    return -1;
+  }
+
+  if (tx_bufs[i])
+  {
+    kfree(tx_bufs[i]);
+    tx_bufs[i] = 0;
+  }
+
+  tx_bufs[i] = buf;
+  tx_ring[i].addr = (uint64)buf;
+  tx_ring[i].length = len;
+  tx_ring[i].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+  tx_ring[i].status &= ~E1000_TXD_STAT_DD;
+  regs[E1000_TDT] = (i + 1) % TX_RING_SIZE;
+  // release(&e1000_lock);
   return 0;
 }
 
@@ -116,10 +137,34 @@ e1000_recv(void)
   // Create and deliver a buf for each packet (using net_rx()).
   //
 
+  // This is where the next packet would go.
+  acquire(&e1000_lock);
+  int i = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+  while (rx_ring[i].status & E1000_RXD_STAT_DD)
+  {
+    // a packet has been received; process it
+    char *buf = rx_bufs[i];
+    int len = rx_ring[i].length;
+
+    // allocate a new buffer to receive the next packet.
+    rx_bufs[i] = kalloc();
+    if (!rx_bufs[i])
+      panic("e1000_recv: kalloc");
+    rx_ring[i].addr = (uint64)rx_bufs[i];
+    rx_ring[i].status = 0;
+
+    // tell the e1000 that this buffer is available
+    regs[E1000_RDT] = i;
+
+    // pass the packet up the stack for processing.
+    net_rx(buf, len);
+
+    i = (i + 1) % RX_RING_SIZE;
+  }
+  release(&e1000_lock);
 }
 
-void
-e1000_intr(void)
+void e1000_intr(void)
 {
   // tell the e1000 we've seen this interrupt;
   // without this the e1000 won't raise any
